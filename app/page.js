9b63febdaf3976e09650656ad33aebe44287c35e
angel <angelpmagallon10@gmail.com>
2025-12-11 21:17:41 -0800

    crop fix

--- app/page.js
+++ app/page.js
@@ -1,31 +1,42 @@
-import React, { FC } from 'react';
+'use client'
 
-const Parser = ({type}) => {
+import React, { FC } from 'react';
+import { useState } from 'react';
+
+const Parser = ({ type, setOutput }) => {
+  const [file, setFile] = useState(null);
+  const updateFile = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleClick = () => {
+    console.log('file: ', file);
+    if(file) {
+      setOutput(file.name);
+    }
+  };
+
   return (
     <div className='parser'>
       <h2>{type}</h2>
-      <input className='upload' type="file" />
-      <button type="submit">Upload</button>
+      <input className='upload' type="file" onChange={updateFile}/>
+      <button onClick={handleClick} type="submit">Upload</button>
     </div>
   );  
 };
 
-const Output = ({output}) => {
-  return (
-    <div className='output'>
-      <h2>Output</h2>
-      <p className='output-text'>{output}</p>
-    </div>
-  );
-}
-
 export default function App() {
+  const [output, setOutput] = useState('nothing to output');
+
   return (
     <>
       <h1>Inhouse Parser</h1>
-      <Parser type="Resume" />
-      <Parser type="Generic" />
-      <Output output={"nothing to output"}/>
+      <Parser type="Resume" setOutput={setOutput}/>
+      <Parser type="Generic" setOutput={setOutput}/>
+      <div className='output'>
+        <h2>Output</h2>
+        <p className='output-text'>{output}</p>
+      </div>
     </>
   );
 }
